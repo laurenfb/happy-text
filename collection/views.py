e94607dd3d4bed8b037ab9a3d0ee79498e965f90
laurenfb <laurenfriesbrink@gmail.com>
2017-02-13 10:15:02 -0800

    nav and templates for about and login

--- collection/views.py
+++ collection/views.py
@@ -2,4 +2,7 @@ from django.shortcuts import render
 
 # Create your views here.
 def index(request):
-    return render(request, 'index.html')
+    number = "303 478 2060"
+    return render(request, 'index.html', {
+        'number': number,
+    })
