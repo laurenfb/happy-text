dd55fddae2a28bbf712f13163bef7a9969898eb8
laurenfb <laurenfriesbrink@gmail.com>
2017-02-15 10:48:38 -0800

    users and quotes created

--- collection/models.py
+++ collection/models.py
@@ -3,3 +3,11 @@ from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
+class User(models.Model):
+    name = models.CharField(max_length=255)
+    slug = models.SlugField(unique=True)
+    number = models.IntegerField()
+
+class Quote(models.Model):
+    author = models.CharField(max_length=255)
+    text = models.TextField()
--- manage.py
+++ manage.py
@@ -2,6 +2,12 @@
 import os
 import sys
 
+from os.path import join, dirname
+from dotenv import load_dotenv
+
+dotenv_path = join(dirname(__file__), '.env')
+load_dotenv(dotenv_path)
+
 if __name__ == "__main__":
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "happytext.settings")
 
